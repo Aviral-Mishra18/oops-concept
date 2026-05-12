03394277f26bb754a852e8c957dbb64539097c95
Aviral Mishra <avi.avimishra.com@gmail.com>
2026-05-12 08:53:06 +0530

    constructor.cpp

--- constructor.cpp
+++ constructor.cpp
@@ -6,7 +6,9 @@ class Teacher {
     private:
     double salary; // private member = not accessible from outside the class..
 
-public: // Access modifier - public means these members are accessible from outside the class..
+public: 
+// Access modifier - public means these members are accessible from outside the class..
+
 //non parameterized constructor..
 // Teacher(){
 //     dept = "Computer Science";
@@ -30,25 +32,34 @@ Teacher(string n, string d, string s,double sal){
     void changeDept(string newDept){
         dept= newDept;
     }
-    //setter = this function use for set private member.
-    void setSalary(double s){
-        salary =s;
-    }
+//     //setter = this function use for set private member.
+//     void setSalary(double s){
+//         salary =s;
+//     }
 
-    //getter = this function use for get private member.
-    double getSalary(){
-        return salary;
-    }
-};
+//     //getter = this function use for get private member.
+//     double getSalary(){
+//         return salary;
+//     }
+
+void getInfo(){
+    cout <<"name: "<< name<< endl;
+    cout<<"subject : "<< subject << endl;
+}
+ };
+
+
 
 int main(){
-    Teacher t1("Aviral, "Computer Science",++",25000);//perimete // constructor call / pehle ye line call hogi..
-    // t1.name = "aviral";
-    // t1.subject = "C++";
-    // t1.setSalary(50000);
+    Teacher t1("Aviral", "Computer Science", "C++", 25000);// parameterized constructor..
+    t1.getInfo();
+
+    // t1.name = "aviral";//{non parameterized contrcutor}
+    // t1.subject = "C++";//{non parameterized contrcutor}
+    // t1.setSalary(50000);//{non parameterized contrcutor}
 
     
-    cout << t1.dept<<endl;
+    // cout << t1.dept<<endl;
     return 0;
 
 }
